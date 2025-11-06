2afe048a15c32b40c52a5e32393982a977fcadcb
Shuruburu <151548902+Shuruburu@users.noreply.github.com>
2025-11-06 17:59:52 +0100

    Add files via upload

--- load.py
+++ load.py
@@ -46,22 +46,98 @@ def Find_the_elements(line, keys):
                    #load the data from the dictionary 
                    print(line[i], end= " ")
                i = i+1
-   return line 
+   return True 
 
-def Write_In_the_file(filename):
-    lines = "The first line"
-    keys = None
-    with open(filename ,  "r") as f :
-         while lines !="":
-             if(lines[0]) !='#':
-                 if keys ==None:
-                     keys=Find_eddit_in_the_file(lines)
-                 else:
-                     Find_the_elements(lines, keys)
-             lines=f.readline()
+def Find_const(f, line, dictionary ):
+    if"const_mix" in line:
+        i=0
+        while  line[i]!= line[-1] and line[i]!= "{":
+            i = i+1
+        if line[i]!= "{" :    
+            #Writting in the file 
+            f.seek(i)
+            f.write(str(dictionary["const_mix"]))
+            return False
+        return True
+    else:
+        return True
+def Find_P_T_profile(line):
+    if "smg" in line:
+        return
+def Find_the_smg(f, line, dictionary):
+    if "use_fix_sp_bot" in line:
+        i =0
+        while line[i]!= line[-1] and line[i]!= "{":
+            i = i+1
+        
+        if line[i]!= "{" :
+            f.seek(i)
+            value = str(dictionary["const_mix"])
+            
+            return False
+        return True
+    else:
+        return True
     
-    return lines
+def Find_pressure(f,line):
+    if"p_b" in line:
+        i=0
+        while line[i].isdigit():
+            i =i+1
+        f.seek(i)
+        f.write()
+        return True
+    else:
+        return False
+def Find_dominat(f, line, dictionary):
+    if"atm_base" in line:
+        i=0
+        while line[i]!= line[-1] and line[i]!= "[":
+            i =i+1
+        if line[i]=="[":
+            f.seek(i)
+            f.write("[{}]".format(dictionary["domiant"]))
+            return False
+        return True
+    else:
+        return True
+##
+#
+def Write_In_the_file(filename, dictionary):
+    Flag = [True, True, True]
+    new_lines = []
 
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if line and line[0] != '#':  # skip commented lines
+            if Flag[0] and "use_fix_sp_bot" in line:
+                Flag[0] = False
+                value = str(dictionary["const_mix"])
+                new_lines.append(f"use_fix_sp_bot = {value}\n")
+                continue
+
+            if Flag[1] and "const_mix =" in line:
+                Flag[1] = False
+                value = str(dictionary["const_mix"])
+                new_lines.append(f"const_mix = {value}\n")
+                continue
+
+            if Flag[2] and "atm_base" in line:
+                Flag[2] = False
+                value = str(dictionary["dominant"])
+                new_lines.append(f"atm_base = '{value}'\n")
+                continue
+
+        # Keep the original line if not modified
+        new_lines.append(line)
+
+    # Overwrite the file with new content
+    with open(filename, "w") as f:
+        f.writelines(new_lines)
+
+    
 def Load_the_excel(filename):
     df= pd.read_excel(filename)
     file = df.to_numpy(dtype=float)
--- main.py
+++ main.py
@@ -20,11 +20,18 @@ def Selection():
     Plots =al.Plots(data)
     keys, columns, values =al.Get_species_elements(data, start_index=0, max_elements=14)
     most = al.classify_the_atmosphere(data, keys, "volume_mixing_ratio")
-    rel_filtered = al.Filter_out_data_relative(data, "volume_mixing_ratio", most)
-    selected = al.Seleciton(rel_filtered)
     
-    preselected=al.Select_unique(selected)
-    return preselected, most
+    filtered_data = al.Filter_out_data(data, "volume_mixing_ratio", tol =-16 )
+    rel_filtered = al.Filter_out_data_relative(data, "volume_mixing_ratio", most)
+    
+    
+    selected = al.Seleciton(rel_filtered)
+    selected1 = al.Seleciton(filtered_data)
+    
+    preselected , preselected1=al.Select_unique(selected, selected1)
+
+
+    return preselected, most, preselected1
 
 def Simulate(keys ,arg):
     # zrobic dwa scrypty, w ktorym jednym dostaje klucze do tablicy a w drugim iteruje po tych kluczach
@@ -42,13 +49,17 @@ if __name__=="__main__":
     Plots =al.Plots(data)
     ratio =al.Element_ratios(data)
     
-    preselected, most = Selection()
-    keys = al.string_fragmentation("'H2O_g' ,'N2_g'" , "'")
-    
-    selcted = al.Selection_of_mixing(preselected)
-    mixing_ratio =  al.Mixing_ratio(data, "volume_mixing_ratio" , selcted)
+    preselected, most, preselected1 = Selection()
+    print(preselected)
+    print("==================================")
+    print(preselected1)
+    selected = al.Selection_of_mixing(preselected1)
+    print(selected)
+    mixing_ratio =  al.Mixing_ratio(data, "volume_mixing_ratio" , selected)
     to_be_writen_file =al.Add_the_domiant_species(mixing_ratio, most)
-    lines = load.Write_In_the_file("/home/shurubura/Documents/VULCAN/vulcan_cfg.py")
+
+    #Here the plan is to pass the dictionary with the number of the atmosphere that we acquired
+    lines = load.Write_In_the_file("/home/shurubura/Documents/VULCAN/vulcan_cfg.py", to_be_writen_file[0])
     
     if sys.argv == "get_the_keys":
         Bash_keys(mixing_ratio.keys())
